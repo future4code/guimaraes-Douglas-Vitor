3d1706d5eae9b21512f0343ba03490edf4228ccb
Douglas Vitor <Dogvicfari@gmail.com>
2022-04-08 22:21:05 -0400

    projeto-labex

--- modulo3/projeto-labex/src/App.js
+++ modulo3/projeto-labex/src/App.js
@@ -1,64 +1,56 @@
-import React, { useState, banana } from 'react';
-import { TelaProdutos } from './TelaProdutos';
+import React from 'react';
+import Playlists from './components/Playlists';
 import styled from 'styled-components';
-import { HarryPotterCharacters } from './HarryPotterCharacters';
+import AdicionarMusicas from './components/AdicionarMusicas';
+import Header from './components/Header';
+import Fundo from './assets/fundocidadenoite.JPG';
 
-function App() {
-  // const pessoa = {
-  //   nome: "iago",
-  //   email: "iago@email.com"
-  // }
-  // const datas = ["2022", "1789"];
 
-  // const retornoDoUseState = useState(0);
-  // const contador = retornoDoUseState[0];
-  // const setContador = retornoDoUseState[1];
 
-  // const [language, setLanguage] = useState("pt");
-
-  // const [esteAno, revolucao] = datas;
-
-  // console.log("O ano atual é", esteAno)
-  // console.log("A revolução francesa foi em", revolucao)
-  // const email = pessoa.email;
-  // const nome = pessoa.nome;
-
-  // const { email } = pessoa;
-
-  // console.log("O nome é",nome);
-  // console.log("O email do iago é",email);
-  // console.log("A profissao é",profissao)
+const Background = styled.div`
+  width: 100%;
+  min-height: 100%;
+  /* background-color: gray; */
+  background-image: url(${Fundo});
+  background-repeat: yellow;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   
-  // const exercicio1 = () => {
-  //   return {
-  //     idade: 28,
-  //     nome: "iago",
-  //     profissao: "instrutor"
-  //   }
-  // }
+`
 
-  // console.log(exercicio1());
 
-  // const { profissao } = exercicio1()
-
-  // console.log("nome:",nome)
-  // console.log("idade",idade)
-  // console.log("profissao",profissao)
-
-  const exercicio2 = () => {
-    return ["chá", "frutas", "pão", "ovo"]
+export default class App extends React.Component {
+  state = {
+    mudarTela: "mostrarPlaylists",
   }
 
-  const [maisImportante, segundoMaisImportante] = exercicio2();
 
-  // console.log("O item mais importante é",maisImportante)
-  // console.log("O segundo item mais importante é", segundoMaisImportante)
-  return (
-    <div>
-      <h1>Custom Hooks - Guimarães</h1>
-      <HarryPotterCharacters />
-    </div>
-  );
-}
+  mudaTela = () => {
+     this.setState({ mudarTela: !this.state.mudarTela});
 
-export default App;
+  }
+
+
+  render() {
+
+    const mudancaDeTela = () => {
+      if (this.state.mudarTela === "mostrarPlaylists") {
+        return <Playlists />
+    } else if (this.state.mudarTela === "mostrarAddMusicas") {
+        return <AdicionarMusicas />
+    }
+  }
+
+    return(
+      <Background>
+
+        <Header />
+        {mudancaDeTela()}
+
+      </Background> 
+    );
+  }
+
+};
